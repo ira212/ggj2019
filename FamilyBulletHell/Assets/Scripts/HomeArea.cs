18c0662f9dc75a0884161dc84f64f32be531261e
Ira Fay <ira@faygames.com>
2019-01-26 14:57:47 -0500

    minor code cleanup

--- FamilyBulletHell/Assets/Scripts/HomeArea.cs
+++ FamilyBulletHell/Assets/Scripts/HomeArea.cs
@@ -4,23 +4,25 @@ using UnityEngine;
 
 public class HomeArea : MonoBehaviour
 {
-
     public System.Action OnHomeAreaEntry;
-    
+    private Collider _homeAreaCollider;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        _homeAreaCollider = gameObject.GetComponent<Collider>();
     }
 
     // Checks if a given game object is within this game object
     public bool Contains (GameObject myObject)
     {
         Collider myObjectCollider = myObject.GetComponent<Collider>();
-        Collider HomeAreaCollider = gameObject.GetComponent<Collider>();
+
         // check if myObject is within myself
-        if (myObjectCollider.bounds.Intersects(HomeAreaCollider.bounds))
+        if (myObjectCollider.bounds.Intersects(_homeAreaCollider.bounds))
+        {
             return true;
+        }
         else
         {
             return false;
