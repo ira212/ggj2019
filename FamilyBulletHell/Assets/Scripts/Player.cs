31b4ac237ed074414b27cf2e1a3c17d5b96f9281
pav17 <pav17@hampshire.edu>
2019-01-26 11:27:01 -0500

    Made player move! Hopefully....

--- FamilyBulletHell/Assets/Scripts/Player.cs
+++ FamilyBulletHell/Assets/Scripts/Player.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+
+    public float speed;
+    private Rigidbody _playerRB;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        _playerRB = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, moveVertical, 0.0f);
+
+        _playerRB.AddForce(movement * speed);
     }
 }
