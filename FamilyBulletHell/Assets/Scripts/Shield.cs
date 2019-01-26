c563a7e475ad374e8af0f43e9d65498b2e6237c5
pav17 <pav17@hampshire.edu>
2019-01-26 10:43:31 -0500

    Added TakeDamage Method to sheild script

--- FamilyBulletHell/Assets/Scripts/Shield.cs
+++ FamilyBulletHell/Assets/Scripts/Shield.cs
@@ -7,16 +7,27 @@ public class Shield : MonoBehaviour
     private int _maxHealth;
     private int _currentHealth;
     private int _respawnTime;
+    private float _respawnTimeLeft;
+    private bool _respawnActive;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void TakeDamage(int damageTaken)
+    {
+        _currentHealth -= damageTaken;
+        if (_currentHealth <= 0)
+        {
+           gameObject.SetActive(false);
+        }
     }
 }
