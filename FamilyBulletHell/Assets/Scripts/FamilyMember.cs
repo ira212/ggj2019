9e847a6b7059acfea1bc2b0f966aa8e0e5c86d3c
Jacob Muehlberg <muehlbergjt@gmail.com>
2019-01-26 11:52:11 -0500

    Change to how FamilyMember handles death. Added parent bool

--- FamilyBulletHell/Assets/Scripts/FamilyMember.cs
+++ FamilyBulletHell/Assets/Scripts/FamilyMember.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class FamilyMember : MonoBehaviour
 {
+    public System.Action OnFamilyMemberDeath;
+
     private int _maxHealth;
     private int _currentHealth;
     private float _speed;
+    private bool _isParent;
 
-    public void SpawnFamilyMember(int maxHP, float moveSpeed)
+    public void SpawnFamilyMember(int maxHP, float moveSpeed, bool isParent)
     {
         _maxHealth = maxHP;
         _currentHealth = maxHP;
         _speed = moveSpeed;
+        _isParent = isParent;
     }
 
     public void TakeDamage(int damageTaken)
@@ -20,7 +24,12 @@ public class FamilyMember : MonoBehaviour
         _currentHealth -= damageTaken;
         if (_currentHealth <= 0)
         {
-            Destroy(gameObject);
+            OnFamilyMemberDeath?.Invoke();
         }
+    }
+
+    public bool IsParent()
+    {
+        return _isParent;
     }
 }
