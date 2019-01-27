94a28aa33e6e721a984262e17d2607805405604f
pav17 <pav17@hampshire.edu>
2019-01-26 19:31:52 -0500

    Updated game over screen to show score

--- FamilyBulletHell/Assets/Scripts/PlayAgain.cs
+++ FamilyBulletHell/Assets/Scripts/PlayAgain.cs
@@ -2,9 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class PlayAgain : MonoBehaviour
-{ 
+{
+
+    public Text scoreTotal;
+
+    void Start()
+    {
+        scoreTotal.text = "Final Score: " + Global.Instance.FinalScore.ToString();
+    }
+
     public void SceneChange()
     {
         Global.Instance.FinalScore = 0;
